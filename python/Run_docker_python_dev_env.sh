d1b89c812a035b45c8ea373bfc2b015bacd9ed30
hbirkdale <hank.birkdale@lendkey.com>
2017-10-21 19:32:40 +0000

    Add TODO at begin of script

--- python/Run_docker_python_dev_env.sh
+++ python/Run_docker_python_dev_env.sh
@@ -1,3 +1,15 @@
+# This script needs some work. The issue is the "DISPLAY" flag below.
+# Technically, DISPLAY is usually set as an env variable on Linux, but not on a Mac
+# and is related to X-Win. What this script needs is:
+# 1. Get the value of $DIPLAY
+# 2. If it's blank - you probably have some work today
+#     - Get the OS
+#       - if Mac - Get the ipaddress
+#               -- Need to also make sure Xquartz is running and xhost + is set to allow connections
+#       - If Linus - Not sure but I think the value is somewhere in the /tmp/.X11-unix fileset
+#     - Set $DISPLAY and replace my hardcoded ip address below
+#     In the interim - if you are running on a Mac, just get your ip address en0 and replace mine below.
+
 docker run --rm \
   -e DISPLAY=192.168.1.5:0 \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
