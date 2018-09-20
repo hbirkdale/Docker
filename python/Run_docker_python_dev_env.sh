b494380dded77d86ad071555f28451597a831a15
hbirkdale <hank.birkdale@gmail.com>
2018-09-20 15:45:36 -0400

    Adding small Linux desktop

--- python/Run_docker_python_dev_env.sh
+++ python/Run_docker_python_dev_env.sh
@@ -11,12 +11,14 @@
 #     In the interim - if you are running on a Mac, just get your ip address en0 and replace mine below.
 
 docker run --rm \
-  -e DISPLAY=192.168.1.5:0 \
+  -e DISPLAY=10.1.10.116:0 \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v /private/tmp/com.apple.launchd.pSRoluUcKy:/private/tmp/com.apple.launchd.pSRoluUcKy \
+  -v ${HOME}/.Xauthority:/home/developer/.Xauthority \
   -v ~/.PyCharm:/home/developer/.PyCharm \
   -v ~/.PyCharm.java:/home/developer/.java \
-  -v ~/.PyCharm.py2:/usr/local/lib/python2.7 \
+  -v ~/.PyCharm.py2:/udockersr/local/lib/python2.7 \
   -v ~/.PyCharm.py3:/usr/local/lib/python3.4 \
   -v ~/Development/Python/:/home/developer \
   --name pycharm-$(head -c 4 /dev/urandom | xxd -p)-$(date +'%Y%m%d-%H%M%S') \
-hbirkdale/python_dev_env:latest
+ python_dev_env:latest
